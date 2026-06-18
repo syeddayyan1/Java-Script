723c4fc801bcd13dd76e171ac4aad9d4c2d049c2
sameerakhtari <sameer_akhtari@hotmail.com>
2026-06-18 16:46:27 +0500

    update projet

--- task.js
+++ task.js
@@ -344,16 +344,44 @@
 // let inp = prompt("Enter Value")
 // alert(typeof(inp))
 
-let inp = prompt("Enter Value")
-if (!isNaN(inp)) {
-    alert( inp+ " is Number")
+// let inp = prompt("Enter Value")
+// if (!isNaN(inp)) {
+//     alert( inp+ " is Number")
+// }
+// else {
+//     alert( inp+ " is " +typeof(inp))
+// }
+
+
+
+// 17. Write a program to create a calculator for +,-,*, / & % using if
+// statements. Take the following input:
+// a. First number
+// b. Second number
+// c. Operation (+, -, *, /, %)
+// Compute & show the calculated result to user.
+
+alert("This is a calculator Program")
+let num1 = prompt("Enter First Number")
+let operator=prompt("Enter operator")
+let num2 = prompt("Enter Second Number")
+
+
+if (operator == "+") {
+    alert(Number(num1) + Number(num2))
 }
-else {
-    alert( inp+ " is " +typeof(inp))
+
+else if (operator == "-") {
+    alert( Number(num1) - Number(num2))
 }
 
+else if (operator == "*") {
+    alert( Number(num1) * Number(num2))
+}
 
-
+else if (operator == "/") {
+    alert( Number(num1) / Number(num2))
+}
 
 
 

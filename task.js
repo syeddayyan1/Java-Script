ceea4c8bc7b8016814249fecf6a1ef988809609a
sameerakhtari <sameer_akhtari@hotmail.com>
2026-06-18 17:14:22 +0500

    update project

--- task.js
+++ task.js
@@ -144,6 +144,9 @@
 // }
 
 
+// ----------------------------------------------------------------------------------------
+
+
 // 11. Guess game:  Store a secret number (ranging from 1 to 10) in a variable. Prompt user to guess the secret number.
 //  a. If user guesses the same number, show  “Bingo! Correct answer”.  b. If the guessed
 // number +1 is the secret number, show  “Close enough to the correct answer”.

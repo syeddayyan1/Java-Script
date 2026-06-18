28a3f16e707f033e27104b3cd5db5abff7dac278
sameerakhtari <sameer_akhtari@hotmail.com>
2026-06-18 18:41:08 +0500

    Update Project

--- array.js
+++ array.js
@@ -1,74 +1,135 @@
-let arr = [2, 3, 4, 5, 6, 8]
+// let arr = [2, 3, 4, 5, 6, 8]
 
-arr[0]= 23
-console.log(arr)
-console.log( arr[2])
-console.log(arr[3])
-console.log(arr.length)
-
-
-let ar = [2, 3, 4, 5]
-ar[1] = 0
-console.log(ar)
-
-// convert in string property
-console.log(arr.toString())
-
-// array joint property
-console.log(arr.join(" and "))
+// arr[0]= 23
+// console.log(arr)
+// console.log( arr[2])
+// console.log(arr[3])
+// console.log(arr.length)
 
 
-// array me se value nikalna
+// let ar = [2, 3, 4, 5]
+// ar[1] = 0
+// console.log(ar)
 
-let array = [1, 2, 4,6,8,23]
-array.pop()
-console.log(array)
+// // convert in string property
+// console.log(arr.toString())
 
-
-
-//  Array me value dalna
-
-let arry = [2, 4, 6, 8]
-arry.push(100);
-console.log(arry)
-// console.log(arry.length)
-
-// array delete
-
-let aary = [9, 6, 8, 3, 5]
- delete arry[0]
-console.log(aary)
-
-// concat aaray Combine
-
-let a1 = [1, 2, 3]
-let a2 = [4, 5, 6]
-let a3 = [7, 8, 9]
-
-console.log(a1.concat(a2, a3))
-
-// sort array sequenc wise 
-
-let b1 = [2,3,1,4,5,7,9,6,0,8]
-console.log(b1.sort())
+// // array joint property
+// console.log(arr.join(" and "))
 
 
-// splice value nikalna
+// ------------------------------------------------------------------------------------------
 
-let numbers = [1, 2, 3, 4, 5]
+
+// // array me se value nikalna
+
+// let array = [1, 2, 4,6,8,23]
+// array.pop()
+// console.log(array)
+
+// ------------------------------------------------------------------------------------------
+
+
+
+// //  Array me value dalna
+
+// let arry = [2, 4, 6, 8]
+// arry.push(100);
+// console.log(arry)
+// // console.log(arry.length)
+
+// ------------------------------------------------------------------------------------------
+
+// // array delete
+
+// let aary = [9, 6, 8, 3, 5]
+//  delete arry[0]
+// console.log(aary)
+
+// ------------------------------------------------------------------------------------------
+
+// // concat aaray Combine
+
+// let a1 = [1, 2, 3]
+// let a2 = [4, 5, 6]
+// let a3 = [7, 8, 9]
+
+// console.log(a1.concat(a2, a3))
+
+// ------------------------------------------------------------------------------------------
+
+// // sort array sequenc wise
+
+// let b1 = [2,3,1,4,5,7,9,6,0,8]
+// console.log(b1.sort())
+
+// ------------------------------------------------------------------------------------------
+
+// // splice value nikalna
+
+// let numbers = [1, 2, 3, 4, 5]
+// numbers.splice(1,2)
+// console.log(numbers)
+
+// // splice value nikal kr dusri value add krnana
+
+// let number = [1, 7, 3, 9, 2]
+// number.splice(1,2,222,333)
+// console.log(number)
+
+// let numberr = [2, 3, 4, 6, 8]
+// numberr.splice(1, 2)
+// console.log(numberr)
+
+// let array = [2, 4, 6, 8, 10]
+// array.splice( 3,1 )
+// console.log(array)
+
+// Array me se "20" aur "30" remove karo.
+
+let numbers = [10, 20, 30, 40, 50];
 numbers.splice(1,2)
 console.log(numbers)
 
-// splice value nikal kr dusri value add krnana
 
-let number = [1, 7, 3, 9, 2]
-number.splice(1,2,222,333)
-console.log(number)
+// ------------------------------------------------------------------------------------------
 
 
-// slice vaalue dena ke kaha se shrow ho or kaha tak jae or un values ko skip krdeta he
+// Mujhe sirf Banana aur Mango chahiye, lekin original array ko touch nahi karna.
+// To hum slice() use karte hain:
 
-let numberr = [2, 3, 4, 6, 8]
-numberr.splice(1, 2)
-console.log(numberr)
+// array = [2, 9, 8, 7, 5]
+// let result= array.slice(1,4)
+// console.log(result)
 
+
+// let fruits = ["Mango", "Banana", "Orange", "Apple", "Stawbery"]
+// let result = fruits.slice(2,4)
+// console.log(result)
+
+
+
+
+// ------------------------------------------------------------------------------------------
+
+// unshift start me value dalna
+
+// let arre = [9, 5, 8, 2, 3, 4]
+// arre.unshift(2)
+// console.log(arre)
+
+
+let colors = ["Green", "Yellow"]
+colors.unshift("red")
+console.log(colors)
+
+// ------------------------------------------------------------------------------------------
+
+
+// shift start se value remove krta he
+
+let array = [0, 1, 2, 3, 4, 5]
+array.shift()
+console.log(array)
+
+// ------------------------------------------------------------------------------------------

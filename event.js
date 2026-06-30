afd466d80a3673b2f58c8006db93196a9af95a75
sameerakhtari <sameer_akhtari@hotmail.com>
2026-06-30 16:25:23 +0500

    Updated Projects

--- event.js
+++ event.js
@@ -1,12 +1,12 @@
 // ON CLICK //
 
-let button = document.getElementById("btn")
+// let button = document.getElementById("btn")
 
-button.addEventListener("click", () => {
-    // alert("i was Clicked Yahhhh!")
-    // document.querySelector(".box").innerHTML="Hey Dayyan"
-    document.body.style.background = "green"
-})
+// button.addEventListener("click", () => {
+//     // alert("i was Clicked Yahhhh!")
+//     // document.querySelector(".box").innerHTML="Hey Dayyan"
+//     document.body.style.background = "green"
+// })
 
 ///////////////////////////////////////////////////////////////
 
@@ -20,27 +20,57 @@ button.addEventListener("click", () => {
 //     document.body.style.background = "green"
 // })
 
+
 ////////////////////////////////////////////////////////
 
-          // click krety he element remove//
+ // click krety he element remove//
 
-let text = document.querySelector(".box")
-text.addEventListener("click", (e)=>{
-    // alert(" I was Clicked")
-    // document.body.style.background="yellow"
-    // console.log(e.target.parentNode);
+// let text = document.querySelector(".box")
+// text.addEventListener("click", (e)=>{
+//     // alert(" I was Clicked")
+//     // document.body.style.background="yellow"
+//     // console.log(e.target.parentNode);
 
-    let remove=document.querySelector("p")
-    remove.remove();
+//     let remove=document.querySelector("p")
+//     remove.remove();
 
-})
+// })
 
 // let button= document.getElementById("btn")
 
 // button.addEventListener("click", ()=>{
 
 //     alert("I was Clicked and i remove Now")
+
     
 //     let remove= document.getElementById("btn")
 //     remove.remove()
+// })
+
+
+
+/////////////////////////////////////////////////////////////////
+
+// Click
+
+function hello() {
+    alert("Yes I was Clicked")
+    
+}
+
+// dbl click
+
+function dbl() {
+    alert("I was Double Clicked")
+}
+
+
+
+
+
+// let button = document.getElementById("btn")
+
+// button.addEventListener("mouseleave", () => {
+//     alert("Yahhhaoooo")
+//     document. getElementById("btn").style.background="yellow"
 // })
